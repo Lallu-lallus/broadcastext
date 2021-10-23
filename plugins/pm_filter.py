3ab623de2d86e17527e07621d9bc69500395bb9f
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-10-23 21:08:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -160,6 +160,7 @@ async def group(client, message):
             parse_mode="html",
             reply_to_message_id=message.message_id
         )
+
         else:
             return
         if not btn:
